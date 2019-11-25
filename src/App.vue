3e874489f98f6a6df089aeb5dc873b1803529071
mgxian <will835559313@163.com>
2019-11-25 15:15:54 +0800

    move code into Counter component

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,16 @@
 <template>
   <div id="app">
-    <span>{{ title }}</span>
-    <br>
-    doubleCount {{ doubleCount() }}
-    <br>
-    count {{ count }}
-    <button @click="handleIncrease">+</button>
-    <button @click="handleDecrease">-</button>
+    <Counter></Counter>
   </div>
 </template>
 
 <script>
-import { mapState, mapMutations, mapGetters, mapActions } from "vuex";
+import Counter from "./components/Counter";
 
 export default {
-  name: "counter",
-  computed: {
-    title: function() {
-      return this.$store.state.title;
-    },
-
-    ...mapState("counter", {
-      count: state => state.count
-    })
-  },
-  methods: {
-    handleIncrease: function() {
-      this.increase();
-    },
-
-    handleDecrease: function() {
-      this.decrease();
-    },
-
-    setTitle: function(title) {
-      this.$store.dispatch("setTitle", title);
-    },
-
-    ...mapMutations("counter", {
-      increaseBy: "increase",
-      decreaseBy: "decrease"
-    }),
-
-    ...mapActions("counter", {
-      increase: "increaseOne",
-      decrease: "decreaseOne"
-    }),
-
-    ...mapGetters("counter", ["doubleCount"])
+  name: "app",
+  components: {
+    Counter
   }
 };
 </script>
-
-<style>
-</style>
---
+++ src/components/Counter.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <span>{{ title }}</span>
+    <br>
+    doubleCount {{ doubleCount() }}
+    <br>
+    count {{ count }}
+    <button @click="handleIncrease">+</button>
+    <button @click="handleDecrease">-</button>
+  </div>
+</template>
+
+<script>
+import { mapState, mapMutations, mapGetters, mapActions } from "vuex";
+
+export default {
+  name: "counter",
+  computed: {
+    title: function() {
+      return this.$store.state.title;
+    },
+
+    ...mapState("counter", {
+      count: state => state.count
+    })
+  },
+  methods: {
+    handleIncrease: function() {
+      this.increase();
+    },
+
+    handleDecrease: function() {
+      this.decrease();
+    },
+
+    setTitle: function(title) {
+      this.$store.dispatch("setTitle", title);
+    },
+
+    ...mapMutations("counter", {
+      increaseBy: "increase",
+      decreaseBy: "decrease"
+    }),
+
+    ...mapActions("counter", {
+      increase: "increaseOne",
+      decrease: "decreaseOne"
+    }),
+
+    ...mapGetters("counter", ["doubleCount"])
+  }
+};
+</script>
+
+<style>
+</style>
